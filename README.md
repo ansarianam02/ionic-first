bca3ad034ebd22fb27272530f68971c42aaf74ee
ansarianam02 <ansari.anam02@gmail.com>
2019-11-05 02:04:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-vzk46m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-vzk46m)
---
+++ pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  styleUrls: [ './home.css' ]
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
